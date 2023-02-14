dfbdaebab1e28f868fd35368650fd91f60d105c0
Alan Nataniel <alannataniel.m@gmail.com>
2023-02-14 09:34:47 -0300

    Configurações

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Olá galera');
+const express = require('express')
+const app = express()
+
+const conf = require('dotenv').config().parsed;
+
+console.log(conf);
+
+const port = conf.HTTPPORT;
+
+app.get('/',(req, res) => {
+    res.send('Hello World')
+}) 
+
+app.listen(port, () => {
+    console.log(`Exemple app listening on port ${port}`)
+})
